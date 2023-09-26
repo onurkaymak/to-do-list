555100be952c3844db77b14aca02371d02808281
onurkaymak <onurkaymak@outlook.com>
2023-09-26 06:17:05 -0700

    create a list to list all to dos, redirect /items to index

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
     }
 
     [HttpGet("/items/new")]
@@ -22,7 +25,7 @@ namespace ToDoList.Controllers
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
-      return View("Index", myItem);
+      return RedirectToAction("Index");
     }
   }
 }
