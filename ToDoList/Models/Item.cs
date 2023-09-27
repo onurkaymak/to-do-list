936b667c2b9e5e380b54dd176be37608f5d7c706
onurkaymak <onurkaymak@outlook.com>
2023-09-26 22:46:53 -0700

    add Find method, add unique id features

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,12 +5,19 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> { };
 
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
 
     public static List<Item> GetAll()
